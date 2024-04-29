2b68eb1ce8da952c004c817d88ec8dd8cc459e8a
Deepak Jacob <deepak.jacob@gmail.com>
2024-04-28 21:26:57 -0400

    new file creation event listener

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use core::panic;
 use std::{
     env,
-    io::{self, Write},
-    sync::Arc,
+    fs::File,
+    io::{self, Read, Write},
+    path::Path,
+    sync::{mpsc::channel, Arc},
 };
 
 use log::debug;
+use notify::{Event, Watcher};
 use url::Url;
 
 struct TransformedData {
@@ -61,12 +64,9 @@ impl Route {
     fn start(location: &str) -> Self {
         let url = Url::parse(location).expect("the source {location} is not valid");
         debug!(
-            "{}",
-            format!(
-                "the scheme of url {} - source is {}",
-                &url.scheme(),
-                &location,
-            )
+            "the scheme of url {} - source is {}",
+            &url.scheme(),
+            &location,
         );
         if url.scheme() == "file" {
             debug!("need to switch on folder listeners !");
@@ -97,12 +97,9 @@ impl Route {
     fn to(mut self, location: &str) -> Self {
         let url = Url::parse(location).expect("the destination {location} is not valid");
         debug!(
-            "{}",
-            format!(
-                "the scheme of url {} - destination is {}",
-                &url.scheme(),
-                &location,
-            )
+            "the scheme of url {} - destination is {}",
+            &url.scheme(),
+            &location,
         );
         self.destination = Some(url);
         self
@@ -111,11 +108,15 @@ impl Route {
 
 struct EIP {
     routes: Vec<Route>,
+    file_paths: Vec<String>,
 }
 
 impl EIP {
     fn new() -> Self {
-        Self { routes: Vec::new() }
+        Self {
+            routes: Vec::new(),
+            file_paths: Vec::new(),
+        }
     }
 
     fn routes(mut self, r: Vec<Route>) -> Self {
@@ -123,47 +124,91 @@ impl EIP {
         self
     }
 
-    fn run(&self) -> io::Result<()> {
+    fn run(&mut self) -> io::Result<()> {
         for route in &self.routes {
-            if let None = &route.destination {
-                panic!(
-                    "for the route with from {}, no to(\"uri\") has been found! ",
-                    &route.source
+            // verify that the destination is provided for the route
+            self.verify_route_destination(route);
+            let source = &route.source;
+            let source_scheme = (&route.source).scheme();
+            if source_scheme == "file" {
+                self.file_paths.push(source.path().to_string());
+                debug!(
+                    "added folder {} to the watch list",
+                    source.path().to_string()
                 );
             }
-            // let extension_filter = &route.extension.clone().unwrap_or_else(|| "*".to_string());
-            // let destination = &route.destination;
+        }
+        let (tx, rx) = channel();
 
-            // for entry in WalkDir::new(&route.location)
-            //     .into_iter()
-            //     .filter_map(Result::ok)
-            //     .filter(|e| e.file_type().is_file())
-            //     .filter(|e| {
-            //         e.path()
-            //             .extension()
-            //             .and_then(|ext| ext.to_str())
-            //             .map(|ext| ext == extension_filter.trim_start_matches('*'))
-            //             .unwrap_or(false)
-            //     })
-            // {
-            //     let mut file = File::open(entry.path())?;
-            //     let mut contents = Vec::new();
-            //     file.read_to_end(&mut contents)?;
-            //     debug!("the size of the contents:      {}", &contents.len());
+        let mut watcher =
+            notify::recommended_watcher(move |res: notify::Result<Event>| match res {
+                Ok(event) => {
+                    // println!("new event {:?}", event);
+                    match event.kind {
+                        notify::EventKind::Create(_) => {
+                            if let Err(e) = tx.send(event) {
+                                debug!("error sending event for {:?}", e);
+                            }
+                        }
+                        _ => {
+                            // We are not interested in any thing other than Create
+                        }
+                    }
+                }
+                Err(e) => debug!("err       {:?}", e),
+            })
+            .unwrap();
 
-            //     let mut transformed_data = TransformedData { data: contents };
+        for folder in &self.file_paths {
+            watcher
+                .watch(Path::new(folder), notify::RecursiveMode::Recursive)
+                .unwrap();
+        }
 
-            //     for transformer in &route.transformers {
-            //         transformed_data = transformer.transform(&transformed_data.data)?;
-            //     }
+        loop {
+            match rx.recv() {
+                Ok(event) => {
+                    if let Some(file_name) = event.paths.get(0) {
+                        debug!("Received: {:?}", file_name);
 
-            //     let output_path = destination.join(entry.file_name());
-            //     let mut output_file = BufWriter::new(File::create(output_path)?);
-            //     output_file.write_all(&transformed_data.data)?;
-            // }
+                        {
+                            // let extension_filter = &route.extension.clone().unwrap_or_else(|| "*".to_string());
+                            // let destination = &route.destination;
+
+                            let mut file = File::open(file_name)?;
+                            let mut contents = Vec::new();
+                            file.read_to_end(&mut contents)?;
+                            debug!("the size of the contents:      {}", &contents.len());
+
+                            let mut transformed_data = TransformedData { data: contents };
+
+                            // for transformer in &route.transformers {
+                            //     transformed_data = transformer.transform(&transformed_data.data)?;
+                            // }
+
+                            // let output_path = destination.join(entry.file_name());
+                            // let mut output_file = BufWriter::new(File::create(output_path)?);
+                            // output_file.write_all(&transformed_data.data)?;
+                        }
+                    }
+                }
+                Err(e) => {
+                    debug!("Receive error: {:?}", e);
+                    break;
+                }
+            }
         }
 
         Ok(())
+    }
+
+    fn verify_route_destination(&self, route: &Route) {
+        if let None = route.destination {
+            panic!(
+                "for the route with from {}, no to(\"uri\") has been found! ",
+                route.source
+            );
+        }
     }
 }
 
@@ -186,17 +231,16 @@ fn main() -> io::Result<()> {
 
     let current_dir = env::current_dir().expect("failed to get current directory");
     debug!("current directory     {:?}", &current_dir);
-
     let input_dir_url =
         Url::from_file_path(current_dir.join("input").canonicalize().unwrap()).unwrap();
     debug!("input directory url   {:?}", &input_dir_url.as_str());
-
     let output_dir_url =
         Url::from_file_path(current_dir.join("output").canonicalize().unwrap()).unwrap();
-    debug!("input directory url   {:?}", &output_dir_url);
+    debug!("output directory url  {:?}", &output_dir_url);
 
     let toy_eip = EIP::new();
     toy_eip
+        // .routes(vec![Route::start(&input_dir_url.as_str())
         .routes(vec![Route::start(&input_dir_url.as_str())
             .extension("csv")
             .stream()
